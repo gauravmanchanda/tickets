1223a3d1b2539b13fc9a067521b6a0b67a5f9f9e
Gaurav Manchanda <gauravmanchanda16@gmail.com>
2017-01-10 21:58:24 +0530

    Added url options in test env. Fixed broken cukes

--- features/step_definitions/sign_up_steps.rb
+++ features/step_definitions/sign_up_steps.rb
@@ -18,5 +18,5 @@ Then(/I should see the sign up form/) do
 end
 
 Then(/I should see the successfully sign up message/) do
-  expect(page).to have_content('Welcome! You have signed up successfully.')
+  expect(page).to have_content('A message with a confirmation link has been sent to your email address. Please follow the link to activate your account.')
 end
