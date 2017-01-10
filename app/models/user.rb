9063e8fa5f7c7b59ab00f74538e80e28dd7b4250
Gaurav Manchanda <gauravmanchanda16@gmail.com>
2017-01-10 17:30:59 +0530

    Added email confirmation when signing up. Added first name presence validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   attr_accessor :create_customer
 
   has_many :comments
 
   before_create :make_customer, if: :create_customer?
+
+  validates :first_name, presence: true
 
   def full_name
     "#{first_name} #{last_name}"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160820145705) do
+ActiveRecord::Schema.define(version: 20170110115822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,10 @@ ActiveRecord::Schema.define(version: 20160820145705) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
